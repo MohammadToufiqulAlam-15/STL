a7d3806e07c527e07e4be2680a1b32451ba7c64e
Toufiq <mdtoufiqulalam64@gmail.com>
2025-05-23 21:39:01 +0600

    initial commit

---
+++ problem_3.cpp
@@ -0,0 +1,16 @@
+//Mohammad Toufiqul Alam
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+ {
+
+  vector<int> v = {5,3,1,2};
+  //descending order
+  sort(v.begin(), v.end());
+
+  for(auto u : v) cout << u <<" ";
+  cout <<endl;
+
+
+ }
