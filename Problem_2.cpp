1ba114ea04c89af6252d64dd568435320697b461
Toufiq <mdtoufiqulalam64@gmail.com>
2025-05-23 21:10:33 +0600

    initial commit

---
+++ Problem_2.cpp
@@ -0,0 +1,11 @@
+//Mohammad Toufiqul Alam
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+     vector<int>v ={2,3,4,5};
+
+     for(int u : v) cout << u <<" ";
+     cout<<endl;
+
+ }
